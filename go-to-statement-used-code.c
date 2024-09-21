9689c18d9c423ad8ce6dcfa59d5ea2d897dca3ab
Isha Negi <itsishanegi@gmail.com>
2024-09-22 03:15:22 +0530

    Add files via upload

---
+++ go-to-statement-used-code.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void main()
+
+{	int a;
+	
+printf("Enter a: ");
+	
+scanf("%d",&a);
+	
+if(a%2==0) 
+{
+	
+		
+goto even;
+	
+}
+	
+else 
+{
+		
+goto odd;
+	
+}
+
+even:
+
+	
+printf(" It is a even number");
+	
+return; 
+odd:
+	
+printf("It is a odd number");
+
+}
